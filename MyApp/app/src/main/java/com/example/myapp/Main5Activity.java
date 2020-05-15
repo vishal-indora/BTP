029c009021be331ff9935eaae43a6b8ffd65723a
martha889 <39094506+martha889@users.noreply.github.com>
2020-05-15 17:51:31 +0530

    Added both graphs

--- MyApp/app/src/main/java/com/example/myapp/Main5Activity.java
+++ MyApp/app/src/main/java/com/example/myapp/Main5Activity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -27,8 +29,45 @@ public class Main5Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
         mChartAg = (LineChart) findViewById(R.id.linechartAg);
         mChartSg = (LineChart) findViewById(R.id.linechartSg);
+
+        Button button_ag = (Button) findViewById(R.id.buttonAg);
+        Button button_sg = (Button) findViewById(R.id.buttonSg);
+
+        button_ag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mChartSg.setVisibility(View.GONE);
+                if (mChartAg.getVisibility() == View.VISIBLE) {
+                    mChartAg.setVisibility(View.GONE);
+                }
+                else {
+                    mChartAg.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        button_sg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mChartAg.setVisibility(View.GONE);
+                if (mChartSg.getVisibility() == View.VISIBLE) {
+                    mChartSg.setVisibility(View.GONE);
+                }
+                else {
+                    mChartSg.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+
+
+
+
+
 
         // X and Y values for graph
         Float[] sample_time_hours = new Float[96];
@@ -50,32 +89,41 @@ public class Main5Activity extends AppCompatActivity {
         }
 
 
+
         LineDataSet setSg = new LineDataSet(yValuesSg, "SG");
         setSg.setFillAlpha(110);
 
-        LineDataSet setAg = new LineDataSet(yValuesSg, "AG");
+        LineDataSet setAg = new LineDataSet(yValuesAg, "AG");
         setAg.setFillAlpha(110);
 
         //Aesthetics
-        setAg.setColor(Color.GREEN);
-        setAg.setCircleColor(Color.RED);
-        setAg.setLineWidth(3f);
+        setAg.setColor(Color.BLUE);
+        setAg.setCircleColor(Color.BLUE);
+        setAg.setLineWidth(4f);
         setAg.setValueTextSize(0);
 
-
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(setAg);
-
-        LineData data = new LineData(dataSets);
-        mChartAg.setData(data);
+        setSg.setColor(Color.RED);
+        setSg.setCircleColor(Color.RED);
+        setSg.setLineWidth(4f);
+        setSg.setValueTextSize(0);
 
 
+        ArrayList<ILineDataSet> dataSetsAg = new ArrayList<>();
+        dataSetsAg.add(setAg);
 
-        // mChart.setOnChartGestureListener(Main5Activity.this);
-        // mChart.setOnChartValueSelectedListener(Main5Activity.this);
+        ArrayList<ILineDataSet> dataSetsSg = new ArrayList<>();
+        dataSetsSg.add(setSg);
+
+        LineData dataAg = new LineData(dataSetsAg);
+        mChartAg.setData(dataAg);
+
+        LineData dataSg = new LineData(dataSetsSg);
+        mChartSg.setData(dataSg);
 
         mChartAg.setDragEnabled(true);
-        mChartAg.setScaleEnabled(false);
+        mChartAg.setScaleEnabled(true);
+        mChartSg.setDragEnabled(true);
+        mChartSg.setScaleEnabled(true);
 
 
 
